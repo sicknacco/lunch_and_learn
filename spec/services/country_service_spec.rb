44702153d2149a4da173e954672fb1a6eb4c0d78
Nick Sacco <nicksacco10@gmail.com>
2023-08-05 11:47:26 -0700

    refactor/test: added string expect statement

--- spec/services/country_service_spec.rb
+++ spec/services/country_service_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe CountryService do
     expect(country).to be_an Array
     expect(country[0]).to have_key :name
     expect(country[0][:name]).to have_key :common
+    expect(country[0][:name][:common]).to be_a String
     expect(country[0][:name][:common]).to eq('Thailand')
   end
 end
