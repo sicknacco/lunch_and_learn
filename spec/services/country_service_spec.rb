30f90e942aae98c7d9bb4225e2d67a92c0300c96
Nick Sacco <nicksacco10@gmail.com>
2023-08-05 14:57:19 -0700

    test: country service get all countries

--- spec/services/country_service_spec.rb
+++ spec/services/country_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe CountryService do
-  it 'can create a connection, and consume the api', :vcr do
+  it 'can create a connection, and get a single country', :vcr do
     country = CountryService.new.get_country('Thailand')
  
     expect(country).to be_an Array
@@ -9,5 +9,15 @@ RSpec.describe CountryService do
     expect(country[0][:name]).to have_key :common
     expect(country[0][:name][:common]).to be_a String
     expect(country[0][:name][:common]).to eq('Thailand')
+  end
+
+  it 'can create a connection, and get all countries', :vcr do
+    countries = CountryService.new.all_countries
+
+    expect(countries).to be_an Array
+    countries.each do |country|
+      expect(country).to have_key :name
+      expect(country[:name]).to have_key :common
+    end
   end
 end
