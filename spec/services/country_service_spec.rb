d3d562f63a8eb35da75a6fda095765f623b2ab7e
Nick Sacco <nicksacco10@gmail.com>
2023-08-05 13:32:45 -0700

    test/refactor: added something

--- spec/services/country_service_spec.rb
+++ spec/services/country_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe CountryService do
   it 'can create a connection, and consume the api', :vcr do
     country = CountryService.new.get_country('Thailand')
-
+ 
     expect(country).to be_an Array
     expect(country[0]).to have_key :name
     expect(country[0][:name]).to have_key :common
