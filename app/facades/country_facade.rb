ad2251a293c7df876b972ad95ce9ab801dfff62e
Nick Sacco <nicksacco10@gmail.com>
2023-08-05 19:44:50 -0700

    refactor and add breadcrumbs

--- app/facades/country_facade.rb
+++ app/facades/country_facade.rb
@@ -1,7 +1,7 @@
 class CountryFacade
   def self.get_country(country)
     if country.nil?
-      country_names = CountryService.new.all_countries.map do |country_data|
+      country_names = CountryService.new.all_countries.map do |country_data| #< all_countries is a method in CountryService
         country_data[:name][:common]
       end
       country_names.sample
