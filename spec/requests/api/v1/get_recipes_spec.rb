bd689c7d876ad807e5551adb3b209f6fa6f2dbfe
Nick Sacco <nicksacco10@gmail.com>
2023-08-06 08:24:46 -0700

    test: returns recipes for a random country tests

--- spec/requests/api/v1/get_recipes_spec.rb
+++ spec/requests/api/v1/get_recipes_spec.rb
@@ -34,4 +34,37 @@ describe 'Recipe API' do
       expect(recipe[:attributes].keys.size).to eq(4)
     end
   end
+
+  it 'can find recipes for a random country', :vcr do
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+
+    get("/api/v1/recipes", headers: headers)
+
+    expect(response).to be_successful
+
+    recipes = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(recipes).to be_a Hash
+    expect(recipes).to have_key :data
+    expect(recipes[:data]).to be_an Array
+
+    recipes[:data].each do |recipe|
+      expect(recipe).to have_key :id
+      expect(recipe[:id]).to eq("null")
+      expect(recipe).to have_key :type
+      expect(recipe[:type]).to eq("recipe")
+
+      expect(recipe).to have_key :attributes
+      expect(recipe[:attributes]).to be_a Hash
+      expect(recipe[:attributes]).to have_key :title
+      expect(recipe[:attributes][:title]).to be_a String
+      expect(recipe[:attributes]).to have_key :image
+      expect(recipe[:attributes][:image]).to be_a String
+      expect(recipe[:attributes]).to have_key :url
+      expect(recipe[:attributes][:url]).to be_a String
+      expect(recipe[:attributes]).to have_key :country
+      
+      expect(recipe[:attributes].keys.size).to eq(4)
+    end
+  end
 end
