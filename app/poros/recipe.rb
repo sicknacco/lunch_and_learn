92cb3a1d02045ba08f3392b98d56262134d0c296
Nick Sacco <nicksacco10@gmail.com>
2023-08-05 19:06:08 -0700

    refactor: add id to recipe poro set at null

--- app/poros/recipe.rb
+++ app/poros/recipe.rb
@@ -2,12 +2,14 @@ class Recipe
   attr_reader :title,
               :url,
               :country,
-              :image
+              :image,
+              :id
 
   def initialize(data, country)
     @title = data[:recipe][:label]
     @url = data[:recipe][:url]
     @country = country
     @image = data[:recipe][:image]
+    @id = 'null'
   end
 end
