3cd225b8809a702c44b0185d660fa4733bddc723
Nick Sacco <nicksacco10@gmail.com>
2023-08-05 17:41:36 -0700

    refactor: singular recipe to plural recipes

--- spec/services/recipe_service_spec.rb
+++ spec/services/recipe_service_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe RecipeService do
   it 'can create a connection, and consume the api', :vcr do
-    recipe = RecipeService.new.get_recipe('Thailand')
+    recipes = RecipeService.new.get_recipes('Thailand')
     
-    expect(recipe).to be_an Hash
-    expect(recipe).to have_key :hits
-    expect(recipe[:hits]).to be_an Array
-    expect(recipe[:hits][0]).to have_key :recipe
-    expect(recipe[:hits][0][:recipe]).to have_key :label
-    expect(recipe[:hits][0][:recipe][:label]).to be_a String  # < Title of recipe
-    expect(recipe[:hits][0][:recipe][:image]).to be_a String  # < Image of recipe
-    expect(recipe[:hits][0][:recipe][:url]).to be_a String  # < Url of recipe
+    expect(recipes).to be_an Hash
+    expect(recipes).to have_key :hits
+    expect(recipes[:hits]).to be_an Array
+    expect(recipes[:hits][0]).to have_key :recipe
+    expect(recipes[:hits][0][:recipe]).to have_key :label
+    expect(recipes[:hits][0][:recipe][:label]).to be_a String  # < Title of recipe
+    expect(recipes[:hits][0][:recipe][:image]).to be_a String  # < Image of recipe
+    expect(recipes[:hits][0][:recipe][:url]).to be_a String  # < Url of recipe
   end
 end
