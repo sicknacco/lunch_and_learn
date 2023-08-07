57fc788c8ae3451725d2980865c3da4e1d86b944
Nick Sacco <nicksacco10@gmail.com>
2023-08-07 08:58:12 -0700

    refactor: fix service tests

--- spec/services/air_quality_spec.rb
+++ spec/services/air_quality_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe AirQualityService do
-  it 'can create a connection, and get air quality data', :vcr do
+  it 'can retrieve air quality data for a capital city', :vcr do
     air_quality = AirQualityService.new.air_quality_data('Nigeria')
 
-    expect(air_quality).to be_an Array
-    air_quality.each do |data|
-      expect(data).to have_key :name
-      expect(data[:name]).to have_key :common # < :common = Country Name
-      expect()
+    expect(air_quality).to be_a Hash
+    expect(air_quality).to have_key :overall_aqi
+    expect(air_quality).to have_key :"PM2.5"
+    expect(air_quality[:"PM2.5"]).to have_key :concentration
+    expect(air_quality).to have_key :CO
+    expect(air_quality[:CO]).to have_key :concentration
     end
   end
 end
