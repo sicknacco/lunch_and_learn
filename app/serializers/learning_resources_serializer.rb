012fb1becf1fbd896e446db2bc9dc99afed8fd94
Nick Sacco <nicksacco10@gmail.com>
2023-08-06 20:17:01 -0700

    feat: set up learning resources serializer

---
+++ app/serializers/learning_resources_serializer.rb
@@ -0,0 +1,23 @@
+class LearningResourcesSerializer
+  def format_resources(country, video, images)
+    {
+      data: {
+        id: nil,
+        type: 'learning_resources',
+        attributes: {
+            country: country,
+            video: {
+              title: video.title,
+              youtube_video_id: video.youtube_video_id,
+              },
+            images: images.map do |image|
+              {
+                alt_tag: image.alt_tag,
+                url: image.url
+              }
+          end
+        }
+      }
+    }
+  end
+end
