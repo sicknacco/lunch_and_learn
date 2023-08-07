29d3a83fad8c0725fad9cfd01561365c4c4d4379
Nick Sacco <nicksacco10@gmail.com>
2023-08-07 13:49:38 -0700

    feat: added secure random for api generation in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
-  
+  before_create :generate_api_key
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates_presence_of :api_key
   validates_uniqueness_of :api_key
   validates :password, presence: true
   has_secure_password
+
+  private
+  def generate_api_key
+    self.api_key = SecureRandom.hex
+  end
 end
