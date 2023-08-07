f00b94913a23160fddc9198c722a5dfff77276e5
Nick Sacco <nicksacco10@gmail.com>
2023-08-07 14:59:34 -0700

    test: finish happy path tests for user creation

--- spec/requests/api/v1/post_user_spec.rb
+++ spec/requests/api/v1/post_user_spec.rb
@@ -21,9 +21,9 @@ describe "Create User API" do
       expect(response.status).to eq(201)
 
       user_data = JSON.parse(response.body, symbolize_names: true)
+      new_user = User.last
 
       expect(User.count).to eq(1)
-
       expect(user_data).to be_a Hash
       expect(user_data).to have_key(:data)
       expect(user_data[:data]).to be_a Hash
@@ -31,9 +31,17 @@ describe "Create User API" do
       expect(user_data[:data]).to have_key(:type)
       expect(user_data[:data][:type]).to eq('user')
       expect(user_data[:data]).to have_key(:id)
-      expect(user_data[:data][:id]).to eq(User.last.id.to_s)
+      expect(user_data[:data][:id]).to eq(new_user.id.to_s)
       expect(user_data[:data]).to have_key(:attributes)
       expect(user_data[:data][:attributes]).to be_a Hash
+
+      expect(user_data[:data][:attributes]).to have_key(:name)
+      expect(user_data[:data][:attributes][:name]).to eq(new_user.name)
+      expect(user_data[:data][:attributes]).to have_key(:email)
+      expect(user_data[:data][:attributes][:email]).to eq(new_user.email)
+
+      expect(user_data[:data][:attributes]).to have_key(:api_key)
+      expect(user_data[:data][:attributes][:api_key]).to eq(new_user.api_key)
     end
   end
 end
