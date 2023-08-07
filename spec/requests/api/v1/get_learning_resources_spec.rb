81d8ab1f10d2244b01f7e617b70c4ee471309e49
Nick Sacco <nicksacco10@gmail.com>
2023-08-06 20:49:50 -0700

    test: complete formatting tests for response happy path

--- spec/requests/api/v1/get_learning_resources_spec.rb
+++ spec/requests/api/v1/get_learning_resources_spec.rb
@@ -13,5 +13,33 @@ describe 'Learning Resource API' do
     expect(resources).to be_a Hash
     expect(resources).to have_key :data
     expect(resources[:data]).to be_a Hash
+
+    expect(resources[:data]).to have_key :id
+    expect(resources[:data][:id]).to eq(nil)
+    expect(resources[:data]).to have_key :type
+    expect(resources[:data][:type]).to eq("learning_resources")
+    expect(resources[:data]).to have_key :attributes
+    expect(resources[:data][:attributes]).to be_a Hash
+
+    expect(resources[:data][:attributes]).to have_key :country
+    expect(resources[:data][:attributes][:country]).to eq(country)
+
+    expect(resources[:data][:attributes]).to have_key :video
+    expect(resources[:data][:attributes][:video]).to be_a Hash
+    expect(resources[:data][:attributes][:video]).to have_key :title
+    expect(resources[:data][:attributes][:video]).to have_key :youtube_video_id
+    expect(resources[:data][:attributes][:video].keys.size).to eq(2)
+
+    expect(resources[:data][:attributes]).to have_key :images
+    expect(resources[:data][:attributes][:images]).to be_an Array
+    expect(resources[:data][:attributes][:images].size).to eq(10)
+
+    resources[:data][:attributes][:images].each do |image|
+      expect(image).to have_key :alt_tag
+      expect(image).to have_key :url
+      expect(image.keys.size).to eq(2)
+    end
+
+    expect(resources[:data][:attributes].keys.size).to eq(3)
   end
 end
