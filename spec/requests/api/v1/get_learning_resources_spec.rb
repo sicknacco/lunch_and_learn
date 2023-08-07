8b7f7f31bcececb43e41c9748d353c1a08109456
Nick Sacco <nicksacco10@gmail.com>
2023-08-06 18:42:18 -0700

    test: begin resource request tests

---
+++ spec/requests/api/v1/get_learning_resources_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Learning Resource API' do
+  it 'can get learning resources for a chosen country', :vcr do
+    country = 'Thailand'
+
+    get "/api/v1/learning_resources?country=#{country}"
+
+    expect(response).to be_successful
+
+    resources = JSON.parse(response.body, symbolize_names: true)
+require 'pry'; binding.pry
+    expect(resources).to be_a Hash
+    expect(resources).to have_key :data
+    expect(resources[:data]).to be_a Hash
+  end
+end
