6e21b47fb39adc9ddb42725cea49f75f1577692c
Nick Sacco <nicksacco10@gmail.com>
2023-08-07 10:23:31 -0700

    refactor to symbols

--- app/serializers/air_quality_serializer.rb
+++ app/serializers/air_quality_serializer.rb
@@ -1,14 +1,14 @@
 class AirQualitySerializer
   def format_air_quality(country)
 
-    { "data":
-      {   "id": nil,
-          "type:": "air_quality",
-          "city": "Abuja",
-          "attributes": 
-            { "aqi": 27,
-              "pm25_concentration": 8.45,
-              "co_concentration": 547.41
+    { data:
+      {   id: nil,
+          type: "air_quality",
+          city: "Abuja",
+          attributes: 
+            { aqi: 27,
+              pm25_concentration: 8.45,
+              co_concentration: 547.41
             } 
       }
     }
--- spec/requests/api/v1/get_air_quality_spec.rb
+++ spec/requests/api/v1/get_air_quality_spec.rb
@@ -11,5 +11,20 @@ describe 'Air Quality API' do
     air_quality = JSON.parse(response.body, symbolize_names: true)
 
     expect(air_quality).to be_a Hash
+    expect(air_quality).to have_key :data
+    expect(air_quality[:data]).to be_a Hash
+
+    expect(air_quality[:data]).to have_key :id
+    expect(air_quality[:data][:id]).to eq(nil)
+    expect(air_quality[:data]).to have_key :type
+    expect(air_quality[:data][:type]).to eq("air_quality")
+    expect(air_quality[:data]).to have_key :city
+    expect(air_quality[:data][:city]).to eq("Abuja")
+    expect(air_quality[:data]).to have_key :attributes
+    expect(air_quality[:data][:attributes]).to be_a Hash
+
+    expect(air_quality[:data][:attributes]).to have_key :aqi
+    expect(air_quality[:data][:attributes]).to have_key :pm25_concentration
+    expect(air_quality[:data][:attributes]).to have_key :co_concentration
   end
 end
