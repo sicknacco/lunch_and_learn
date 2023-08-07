ffbc14d4f8a372eeda96aad942aeb68eadc0e6ef
Nick Sacco <nicksacco10@gmail.com>
2023-08-06 17:54:23 -0700

    feat: image facade completed

--- app/facades/image_facade.rb
+++ app/facades/image_facade.rb
@@ -1,7 +1,7 @@
 class ImageFacade
   def country_images(country)
     images = ImageService.new.get_images(country)
-    images.map do |image|
+    images[:results].map do |image|
       Image.new(image)
     end
   end
