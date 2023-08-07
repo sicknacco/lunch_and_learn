ebb75e13560ad66e79751ae7b695fdcf5ec1fe23
Nick Sacco <nicksacco10@gmail.com>
2023-08-06 17:54:02 -0700

    test: image facade tests complete

--- spec/facades/image_facade_spec.rb
+++ spec/facades/image_facade_spec.rb
@@ -5,7 +5,23 @@ RSpec.describe ImageFacade, :vcr do
     it 'returns an array of images' do
       country = 'Thailand'
       images = ImageFacade.new.country_images(country)
+
       expect(images).to be_an Array
+    end
+
+    it 'returns all of the correct attributes' do
+      country = 'Thailand'
+      images = ImageFacade.new.country_images(country)
+
+      expect(images).to all(be_a(Image))
+      expect(images[0].alt_tag).to be_a String
+      expect(images[0].url).to be_a String
+    end
+
+    it 'returns an empty array if no images are found' do
+      country = 'bigfootville'
+      images = ImageFacade.new.country_images(country)
+      expect(images).to eq([])
     end
   end
 end
