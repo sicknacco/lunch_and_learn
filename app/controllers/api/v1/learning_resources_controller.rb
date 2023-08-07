4e0a043a82a9ca33d54ea101883b72701b5444b6
Nick Sacco <nicksacco10@gmail.com>
2023-08-07 07:33:56 -0700

    feat: conditional added to learning controller for sad path

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::LearningResourcesController < ApplicationController
   def index
     video = VideoFacade.new.video(params[:country])
     images = ImageFacade.new.country_images(params[:country])
-    render json: LearningResourcesSerializer.new.format_resources(params[:country], video, images)
+    if video.nil? || images.empty?
+      render json: LearningResourcesSerializer.new.format_empty_resources(params[:country])
+    else
+      render json: LearningResourcesSerializer.new.format_resources(params[:country], video, images)
+    end
   end
 end
