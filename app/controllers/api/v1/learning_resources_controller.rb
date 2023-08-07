73d7e69d1cff9ebc1e5291cdeabe73511214ef6f
Nick Sacco <nicksacco10@gmail.com>
2023-08-06 20:18:04 -0700

    feat: controller wip

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::LearningResourcesController < ApplicationController
   def index
     video = VideoFacade.new.video(params[:country])
     images = ImageFacade.new.country_images(params[:country])
-    render json: LearningResourceSerializer.format_resources(params[:country], video, images)
+    render json: LearningResourcesSerializer.new.format_resources(params[:country], video, images)
   end
 end
