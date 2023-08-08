ce921dd5c1652ebc23601ddcdb2a2e94b19373f8
Nick Sacco <nicksacco10@gmail.com>
2023-08-08 14:06:47 -0700

    feat/refactor: create favorite model and add test to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,19 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of :api_key }
     it { should validate_presence_of :password }
   end
+
+  describe '#generate_api_key' do
+    it 'can generate an api key' do
+
+      user = User.new(name: 'Bob',
+        email: 'bob@bob.com',
+        password: 'password', 
+        password_confirmation: 'password')
+
+      user.generate_api_key
+
+      expect(user.api_key).to_not be_nil
+      expect(user.api_key).to be_a String
+    end
+  end
 end
