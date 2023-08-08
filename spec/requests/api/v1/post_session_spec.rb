5bff5032e2cf388e88e59b0809f35f49bb08d398
Nick Sacco <nicksacco10@gmail.com>
2023-08-07 20:33:26 -0700

    test: session request tests started

--- spec/requests/api/v1/post_session_spec.rb
+++ spec/requests/api/v1/post_session_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Create Sessions API" do
+  before(:each) do
+    User.create!(name: 'Bob', email: 'bob@bob.com', password: 'password', password_confirmation: 'password', api_key: '123456789')
+  end
 
+  describe 'POST /api/v1/sessions' do
+    it 'can create a new session' do
+      payload = {
+        email: 'bob@bob.com',
+        password: 'password'
+      }
+      headers = {
+        "CONTENT_TYPE" => "application/json",
+        "ACCEPT" => "application/json"
+      }
+      post "/api/v1/sessions", headers: headers, params: JSON.generate(payload)
+
+      expect(response).to be_successful
+      
+    end
+  end
 end
