34afcedbac318b837018086592e773b849b2c8ab
Nick Sacco <nicksacco10@gmail.com>
2023-08-08 08:41:40 -0700

    test: begin book facade tests

---
+++ spec/facades/books_facade_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe BooksFacade do
+  describe 'class methods' do
+    describe '.get_books' do
+      it 'returns an array of book objects', :vcr do
+        books = BooksFacade.get_books('bangkok', 5)
+
+        expect(books).to be_an Array
+        expect(books.first).to be_a Book
+      end
+    end
+  end
+end
