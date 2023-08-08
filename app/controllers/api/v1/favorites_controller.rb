8f8f00314332e78934a2207b56581519fe805d8b
Nick Sacco <nicksacco10@gmail.com>
2023-08-08 13:02:16 -0700

    feat: route to controller creation and action

---
+++ app/controllers/api/v1/favorites_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::FavoritesController < ApplicationController
+  def create
+    @user = User.find_by(api_key: params[:api_key])
+    favorite = Favorite.new(favorite_params)
+    # favorite = @user.favorites.new(favorite_params)
+    # require 'pry'; binding.pry
+  end
+
+
+  private
+  def favorite_params 
+    params.permit(:country, :recipe_link, :recipe_title)
+  end
+end
