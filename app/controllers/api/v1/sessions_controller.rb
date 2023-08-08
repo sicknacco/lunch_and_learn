c2ecd5192dc752e84dcf62dc97183a54a5296bd4
Nick Sacco <nicksacco10@gmail.com>
2023-08-07 21:01:21 -0700

    feat: happy path added to create action in session controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::SessionsController < ApplicationController
   def create
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      render json: UserSerializer.new(user), status: 200
+    end
   end
 end
