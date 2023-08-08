3aa450a482433c6bb2f407e886309358936fb020
Nick Sacco <nicksacco10@gmail.com>
2023-08-08 13:01:55 -0700

    test/refactor: change name to title

--- spec/requests/api/v1/post_favorites_spec.rb
+++ spec/requests/api/v1/post_favorites_spec.rb
@@ -2,21 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "Post Favorites API" do
   before(:each) do
-    before(:each) do
-      @user = User.create!(name: 'Bob',
-                          email: 'bob@bob.com',
-                          password: 'password', 
-                          password_confirmation: 'password',
-                          api_key: '123456789')
-    end
+    @user = User.create!(name: 'Bob',
+                        email: 'bob@bob.com',
+                        password: 'password', 
+                        password_confirmation: 'password',
+                        api_key: '123456789')
+  end
 
   describe 'POST /api/v1/favorites' do
     it 'can add a favorite for a user', :vcr do
       payload = {
         api_key: @user.api_key,
         country: 'Thailand',
-        recipe_link: 'https://www.allrecipes.com/recipe/87872/sweet-chili-thai-sauce/'
-        recipe_name: 'Sweet Chili Thai Sauce'
+        recipe_link: 'https://www.allrecipes.com/recipe/87872/sweet-chili-thai-sauce/',
+        recipe_title: 'Sweet Chili Thai Sauce'
       }
       headers = {
         "CONTENT_TYPE" => "application/json",
