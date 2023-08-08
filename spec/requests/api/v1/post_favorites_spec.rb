b1d0a9d52b4bc67cc509be75e289d66a0ddadf94
Nick Sacco <nicksacco10@gmail.com>
2023-08-08 12:46:15 -0700

    test: wip

--- spec/requests/api/v1/post_favorites_spec.rb
+++ spec/requests/api/v1/post_favorites_spec.rb
@@ -16,7 +16,22 @@ RSpec.describe "Post Favorites API" do
         api_key: @user.api_key,
         country: 'Thailand',
         recipe_link: 'https://www.allrecipes.com/recipe/87872/sweet-chili-thai-sauce/'
+        recipe_name: 'Sweet Chili Thai Sauce'
       }
+      headers = {
+        "CONTENT_TYPE" => "application/json",
+        "ACCEPT" => "application/json"
+      }
+
+      post "/api/v1/favorites", headers: headers, params: JSON.generate(payload)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      favorite_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(Favorite.count).to eq(1)
+      expect(favorite_data).to be_a Hash
     end
   end
 end
