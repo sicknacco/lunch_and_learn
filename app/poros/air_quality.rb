22a98ed5987cf919bca19b481544df6e8ce90cd1
Nick Sacco <nicksacco10@gmail.com>
2023-08-09 12:35:04 -0700

    comment out facade and poro not needed yet

--- app/poros/air_quality.rb
+++ app/poros/air_quality.rb
@@ -1,13 +1,13 @@
-class AirQuality
-  attr_reader :country,
-              :aqi,
-              :pm25_concentration,
-              :co_concentration
+# class AirQuality
+#   attr_reader :country,
+#               :aqi,
+#               :pm25_concentration,
+#               :co_concentration
 
-  def initialize(data, country)
-    @country = country
-    @aqi = data[:overall_aqi]
-    @pm25_concentration = data[:"PM2.5"][:concentration]
-    @co_concentration = data[:CO][:concentration]
-  end
-end
+#   def initialize(data, country)
+#     @country = country
+#     @aqi = data[:overall_aqi]
+#     @pm25_concentration = data[:"PM2.5"][:concentration]
+#     @co_concentration = data[:CO][:concentration]
+#   end
+# end
